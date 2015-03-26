8e5cf680f8b2975dea5b6bf2f4bac802783c3e57
mick <mmailm@mail.ru>
2015-03-27 01:43:00 +0800

    fix common bugs, add config.yml, add windows support

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,7 @@
+import yaml
 import application.views
 import application.models
 import application.app_settings
 import application.assets_settings
+
+
--- application/app_settings.py
+++ application/app_settings.py
@@ -1,13 +1,36 @@
 import os
 from flask import Flask
+import yaml
+import platform
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+config = {}
+try:
+    with open(os.path.join(ROOT_DIR, 'config.yml')) as f:
+        config = yaml.load(f)
+except BaseException as e:
+    print e
 
 app = Flask(__name__)
 app.debug = True
 app.threaded = True
 app.config['SECRET_KEY'] = "!secret"
-app.config['SAVE_PATH'] = '/mnt/homeworld/_IMAGES/_PPL' # os.path.join(ROOT_DIR, "media")
+
+print config
+
+if 'SAVE_PATH' in config:
+    save_path = config['SAVE_PATH']
+    if platform.system() == 'Windows':
+        save_path = save_path.get('win', '.')
+    else:
+        save_path = save_path.get('unix', '.')
+else:
+    save_path = '.'
+
+print save_path
+
+app.config['SAVE_PATH'] = save_path
 
 
 
