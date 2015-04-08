a12c339aca4e5c21f5f85a6a20bc9709c5589ee8
mick <mmailm@mail.ru>
2015-04-08 13:24:29 +0800

    add flag to open browser on loading

--- run.bat
+++ run.bat
@@ -1,2 +1,1 @@
-python theplace.py
-start http://127.0.0.1:5000/
+python theplace.py w
--- theplace.py
+++ theplace.py
@@ -1,21 +1,17 @@
-import os
-import random
 import threading
 import webbrowser
+import sys
 from flask.ext.migrate import MigrateCommand, Migrate
 from flask.ext.script import Manager
 
 from application.app_settings import app, ROOT_DIR
-from application.models import db
-# from application.socket_settings import socketio
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    # socketio.run(app)
-    if not app.debug:
-        port = 5000 + random.randint(0, 999)
+    if 'w' in sys.argv:
+        port = 5000  # + random.randint(0, 999)
         url = "http://127.0.0.1:{0}".format(port)
         threading.Timer(1.25, lambda: webbrowser.open(url)).start()
         app.run(port=port, threaded=True)
